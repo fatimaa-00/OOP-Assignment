14ddd7edc9435e6e2e85f6d868a74a5394a2b0a2
Fatima-001 <115479531+Fatima-001@users.noreply.github.com>
2022-10-26 21:04:35 +0500

    Add files via upload

---
+++ escape/escape.java
@@ -0,0 +1,52 @@
+// \t -> It gives a tab between two words.
+
+class escape {
+
+    public static void main(String[] args)
+
+    {
+
+        System.out.println("This is lab\t 7a! ");
+
+// This \n escape sequence is for a new line.
+
+        System.out.println("Hello! \nThis is Fatima  ");
+
+// The escape sequence \b is a backspace character
+
+// It moves the cursor one character back with
+
+// or without deleting the character (depending upon compiler)
+
+        System.out.println("Good Morning\bg classmates! ");
+
+// This \r escape sequence is a carriage return character
+
+// It moves the output point back to the beginning of the line without moving down a line (usually).
+
+        System.out.println("Good Morning guys! \r How are you all? ");
+
+// This \f escape sequence is a form feed character
+
+// It is an old technique and used to indicate a page break.
+
+        System.out.println("Good Morning! \f How are you all?  ");
+
+// This \' escape sequence is for printing a single quotation mark on the text string
+
+        System.out.println("Wake up \'Pakistan!\' wake up?  ");
+
+// This \\ escape sequence is for printing a backslash on the text string
+
+        System.out.println("\\- this is a backslash. ");
+
+    }
+
+}
+
+
+
+
+
+
+
